435c3c05cb7ad9621b0fe67e1421906acfffdb0d
minalsabir17 <ms15639@nyu.edu>
2025-05-30 14:05:33 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler  
+
+# Initialize app with PROPER token format
+app = App(token="SLACK_TOKEN")  
+
+@app.message("hello")
+def say_hello(message, say): 
+    say(f"Hi <@{message['user']}>!")  
+
+if __name__ == "__main__": 
+    handler = SocketModeHandler(
+        app, 
+        "SLACK_APP_TOKEN" 
+    )
+    handler.start()
