16bf869cfc9e1902e97dae340442769c008893b7
Bhagawat Chapagain <bchapaga@stevens.edu>
2025-06-07 14:00:42 -0500

    dotenv change

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import os
+from dotenv import dotenv_values
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler  
 
-app = App(token="SLACK_TOKEN")  
+config = dotenv_values(".env")
+SLACK_TOKEN = config['SLACK_TOKEN']
+SLACK_APP_TOKEN = config['SLACK_APP_TOKEN']
+
+
+app = App(token=SLACK_TOKEN)  
 
 @app.message("hello")
 def say_hello(message, say): 
@@ -11,6 +17,6 @@ def say_hello(message, say):
 if __name__ == "__main__": 
     handler = SocketModeHandler(
         app, 
-        "SLACK_APP_TOKEN" 
+        SLACK_APP_TOKEN
     )
     handler.start()
